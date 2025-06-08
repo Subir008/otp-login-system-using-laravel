583c95908a9fe7f40e321a4fc797f20c8fad43a9
Subir008 <subirkarmakar8aug@gmail.com>
2025-06-08 19:55:16 +0530

    code update

---
+++ resources/views/error.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <img src="/assets/img/404-error.jpg" alt="" width="100%" height="100%">
+</div>
--- routes/web.php
+++ routes/web.php
@@ -7,28 +7,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function(){
     
-    Route::view('signup', 'signup')->name('signup');
-    Route::view('login', 'login')->name('login');
-
+    Route::get('/' , function(){
+        return redirect('login');
+    });
     Route::controller(AuthController::class)->group(function () {
+        Route::get('login', 'login_pg')->name('login');
+        Route::get('signup', 'signup_pg')->name('signup');
         Route::post('signup' , 'signup')->name('signup');
         Route::post('login' , 'login')->name('login');
     });
     
 });
 
+// 
 Route::group(['middleware' => 'auth'] , function(){
-    Route::view('home' , 'home')->name('home');
+    Route::get('home' , 'home')->name('home');
 
     Route::controller(AuthController::class)->group(function () {
         Route::get('logout', 'logout')->name('logout');
     });
 });
 
-
+// Otp Handling Controller
 Route::controller(OtpAuthController::class)->group(function(){
     Route::get('otp-login', 'login')->name('otplogin');
     Route::post('otp-generate','generate')->name('generate');
     Route::get('otp-verification/{user_id}'  , 'verification')->name('otpVerification');
     Route::post('otp-verify' , 'loginWithOtp')->name('otpVerify');
+});
+
+// Error page redirect
+Route::fallback(function(){
+    return view('error');
 });
